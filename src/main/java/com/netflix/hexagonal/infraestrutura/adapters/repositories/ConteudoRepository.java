638433b653f75dc7d4ff6cc8589a34dbcafb2232
DiegoMacedo <diegomacedo_cp@hotmail.com>
2022-05-09 22:51:41 -0300

    Corrigindo erro no banco mongo

--- src/main/java/com/netflix/hexagonal/infraestrutura/adapters/repositories/ConteudoRepository.java
+++ src/main/java/com/netflix/hexagonal/infraestrutura/adapters/repositories/ConteudoRepository.java
@@ -2,6 +2,7 @@ package com.netflix.hexagonal.infraestrutura.adapters.repositories;
 
 import com.netflix.hexagonal.domain.models.Conteudo;
 import com.netflix.hexagonal.domain.ports.repositories.ConteudoRepositoryPort;
+import com.netflix.hexagonal.infraestrutura.adapters.modelsDB.ConteudoDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 @Profile("prod")
@@ -19,34 +21,42 @@ public class ConteudoRepository implements ConteudoRepositoryPort {
 
     @Override
     public List<Conteudo> buscarTodos() {
-        return this.springConteudoRepository.findAll();
+        List<ConteudoDB> conteudoEntities = this.springConteudoRepository.findAll();
+        return conteudoEntities.stream().map(ConteudoDB::toConteudo).collect(Collectors.toList());
     }
 
     @Override
     public Conteudo buscarId(Long id) {
-        Optional<Conteudo> conteudoEntity = this.springConteudoRepository.findById(id);
+        Optional<ConteudoDB> conteudoEntity = this.springConteudoRepository.findById(id);
 
         if (conteudoEntity.isPresent())
-            return conteudoEntity.get();
+            return conteudoEntity.get().toConteudo();
 
         return null;
     }
 
     @Override
     public Conteudo salvar(Conteudo conteudo) {
-        if (!Objects.isNull(conteudo.getId()))
-            conteudo = this.springConteudoRepository.findById(conteudo.getId()).get();
+        ConteudoDB conteudoDB;
+        if (Objects.isNull(conteudo.getId()))
+            conteudoDB = new ConteudoDB(conteudo);
+        else {
+            conteudoDB = this.springConteudoRepository.findById(conteudo.getId()).get();
+            conteudoDB.atualizar(conteudo);
+        }
 
-        return this.springConteudoRepository.save(conteudo);
+        return this.springConteudoRepository.save(conteudoDB).toConteudo();
     }
 
     @Override
     public List<Conteudo> obterConteudoPorRangeDeReleaseYear(Integer de, Integer ate) {
-        return this.springConteudoRepository.obterConteudoPorRangeDeReleaseYear(de,ate);
+        List<ConteudoDB> conteudoEntities = this.springConteudoRepository.obterConteudoPorRangeDeReleaseYear(de, ate);
+        return conteudoEntities.stream().map(ConteudoDB::toConteudo).collect(Collectors.toList());
     }
 
     @Override
     public List<Conteudo> buscarPorTipo(String tipo) {
-        return this.springConteudoRepository.buscarPorTipo(tipo);
+        List<ConteudoDB> conteudoEntities = this.springConteudoRepository.buscarPorTipo(tipo);
+        return conteudoEntities.stream().map(ConteudoDB::toConteudo).collect(Collectors.toList());
     }
 }
--- src/main/java/com/netflix/hexagonal/infraestrutura/adapters/repositories/SpringConteudoRepository.java
+++ src/main/java/com/netflix/hexagonal/infraestrutura/adapters/repositories/SpringConteudoRepository.java
@@ -1,6 +1,7 @@
 package com.netflix.hexagonal.infraestrutura.adapters.repositories;
 
 import com.netflix.hexagonal.domain.models.Conteudo;
+import com.netflix.hexagonal.infraestrutura.adapters.modelsDB.ConteudoDB;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,13 +9,12 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface SpringConteudoRepository extends MongoRepository<Conteudo, Long> {
+public interface SpringConteudoRepository extends MongoRepository<ConteudoDB, Long> {
 
     @Query("{ $and: [ { 'release_year': { $gt: ?0 } }, { 'release_year': { $lt: ?1 } } ] }")
-    List<Conteudo> obterConteudoPorRangeDeReleaseYear(Integer de, Integer ate);
+    public List<ConteudoDB> obterConteudoPorRangeDeReleaseYear(Integer de, Integer ate);
 
     @Query("{ type: ?0 }")
-    List<Conteudo> buscarPorTipo(String tipo);
+    public List<ConteudoDB> buscarPorTipo(String tipo);
 
 }
-
